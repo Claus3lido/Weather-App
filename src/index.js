aa9b4dd718f2470a2e38cbf406a44215e708f994
Claus3lido <cumplido26@gmail.com>
2022-09-20 05:35:05 -0700

    updating font size

--- src/index.js
+++ src/index.js
@@ -60,7 +60,7 @@ function displayForecast(response) {
           forecastDay.weather[0].icon
         }@2x.png"
         alt=""
-        width="48"
+        width="50"
       />
       <div class="weather-forcast-temperatures">
         <span class="weather-forecast-temperature-max"> ${Math.round(
